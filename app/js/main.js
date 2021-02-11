f602044719149b74449cc5f1065bd5af1e457f8f
artemdiduk <artem_didyk@ukr.net>
2021-02-11 11:42:30 +0200

    add slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,21 @@
 $(function(){
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        readOnly: true,
+        starWidth: "12px",
+    });
+
+ 
+
+    $('.product-slider__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+        arrows: false
+    });
+
+
     var mixer = mixitup('.products__inner-box');
-    
+
 });
